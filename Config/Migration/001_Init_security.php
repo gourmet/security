103ef3967362e1cf3ef02c6b8dbe22e64f462773
Jad Bitar <jadbitar@mac.com>
2014-02-09 14:03:57 +0200

    Fix `acoid` and `aroid` columns.

--- Config/Migration/001_Init_security.php
+++ Config/Migration/001_Init_security.php
@@ -38,8 +38,8 @@ class InitSecurity extends CakeMigration {
 					'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
 					'aro' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36),
 					'aco' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36),
-					'aroid' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36),
-					'acoid' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36),
+					'aroid' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36),
+					'acoid' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36),
 					'expires' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
 					'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
 					'indexes' => array(
